296d3b866028674a75d7aabe6fe2bfbc736e4b0b
puchochiri <doawishfor@naver.com>
2025-10-29 22:43:23 +0900

    commit

---
+++ src/main/java/org/puchori/springbootproject/security/handler/CustomSocialLoginSuccessHandller.java
@@ -0,0 +1,49 @@
+package org.puchori.springbootproject.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.puchori.springbootproject.config.PasswordEncoderConfig;
+import org.puchori.springbootproject.dto.MemberSecurityDTO;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+@Log4j2
+@RequiredArgsConstructor
+public class CustomSocialLoginSuccessHandller implements AuthenticationSuccessHandler {
+
+    private final PasswordEncoder passwordEncoder;
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        log.info("--------------------------------------------");
+        log.info("CustomLoginSucessHandler onAuthenticationSuccess .................");
+        log.info(authentication.getPrincipal());
+
+        MemberSecurityDTO memberSecurityDTO = (MemberSecurityDTO) authentication.getPrincipal();
+
+        String encodedPw = memberSecurityDTO.getMpw();
+
+        //소셜 로그인이고 ghldnjsdml votmdnjemrk 1111
+
+        if(memberSecurityDTO.isSocial() && (memberSecurityDTO.getMpw().equals("1111") || passwordEncoder.matches("1111",memberSecurityDTO.getMpw())))
+        {
+            log.info("Should Change Password");
+
+            log.info("Redirect to Member Modify ");
+
+            response.sendRedirect("/member/modify");
+
+            return;
+        } else {
+            response.sendRedirect("/board/list");
+        }
+
+
+    }
+}
